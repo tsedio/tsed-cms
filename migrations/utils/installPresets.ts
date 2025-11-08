97b32e37e2b1597ec6ecb3e1570d6f7dbd8936f9
Romain Lenzotti <rom.lenzotti@gmail.com>
2025-11-08 08:48:39 +0100

    fix: update presets

--- migrations/utils/installPresets.ts
+++ migrations/utils/installPresets.ts
@@ -1,20 +1,71 @@
 import { useLogger } from "@directus/api/logger/index";
 import type { Preset } from "@directus/types";
 import { Knex } from "knex";
+import { v4 } from "uuid";
 
 const TABLE = "directus_presets";
 
 export async function installPresets(knex: Knex, presets: Preset[]): Promise<void> {
   const logger = useLogger();
+
+  try {
+    const hasUuid = await knex.schema.hasColumn("directus_presets", "uuid");
+
+    if (!hasUuid) {
+      await knex.schema.alterTable("directus_presets", (table) => {
+        table.uuid("uuid").nullable();
+      });
+      const row: Record<string, any> = {
+        collection: "directus_presets",
+        field: "uuid",
+        special: "uuid",
+        interface: "input",
+        readonly: true,
+        required: false,
+        hidden: true,
+        sort: 1,
+        width: "full"
+      };
+      await knex("directus_fields").insert(row);
+    }
+  } catch (error) {
+    logger.error(`Error altering directus_presets or directus_fields table:`, error);
+    throw error;
+  }
+
   // For each language, check if it exists and insert it if it doesn't
   for (const preset of presets) {
-    const exists = await knex(TABLE).where({ collection: preset.collection, role: null, user: null }).first();
+    const [exists, uuidExists] = await Promise.all([
+      knex(TABLE).where({ collection: preset.collection, role: null, user: null }).first(),
+      knex(TABLE)
+        .where({ collection: preset.collection, uuid: (preset as any).uuid })
+        .first()
+        .catch(() => false)
+    ]);
 
-    if (!exists) {
+    if (!exists && !uuidExists) {
       logger.info(`Adding preset: ${preset.collection}`);
-      await knex(TABLE).insert(preset);
+      try {
+        (preset as any).uuid = v4();
+
+        await knex(TABLE).insert(preset);
+      } catch (er) {
+        delete (preset as any).uuid;
+        await knex(TABLE).insert(preset);
+      }
     } else {
-      logger.info(`Preset already exists: ${preset.collection}`);
+      logger.info(`Update preset: ${preset.collection}`);
+      if (uuidExists) {
+        await knex(TABLE)
+          .update(preset as any)
+          .where({ uuid: uuidExists.uuid });
+      } else {
+        (preset as any).uuid = v4();
+
+        await knex(TABLE)
+          .update(preset as any)
+          .where({ id: exists.id });
+      }
     }
   }
 
